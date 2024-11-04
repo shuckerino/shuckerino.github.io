7c7a77310ecd68d60e9db677a0f980bf6a20c657
Schuster, Rinaldo <rino.schu@gmail.com>
2024-11-04 09:53:23 +0100

    Added simple template

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
